800b8a2712de1a5d328ab6e6c7758558c1b2c743
Navjot Singh Virk <virksaabnavjot@gmail.com>
2021-09-12 21:03:36 +0100

    Create server-backup.js

---
+++ server-backup.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+const express = require('express');
+const server = express();
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000')
+});
+
+server.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+  const event = request.body; 
+  //console.log(event);
+  
+  response.send();
+  
+  axios.post('https://hooks.slack.com/services/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', {
+  "blocks":[
+     {
+        "type":"section",
+        "text":{
+           "type":"mrkdwn",
+           "text": " *"+event.head_commit.author.name+"* pushed a commit to "+event.repository.full_name+"\n\n "+event.head_commit.url+ ":tada:"
+        }
+     },
+     {
+        "type":"actions",
+        "elements":[
+           {
+              "type":"button",
+              "text":{
+                 "type":"plain_text",
+                 "text":"View Changes",
+                 "emoji":true
+              },
+              "value":"click_me_123",
+              "url": event.head_commit.url
+           },
+          {
+              "type":"button",
+              "text":{
+                 "type":"plain_text",
+                 "text":"View Repository",
+                 "emoji":true
+              },
+              "value":"click_me_123",
+              "url": event.url
+           }
+        ]
+     }
+  ]
+})
+.then(function (response) {
+  //console.log(response);
+})
+.catch(function (error) {
+  //console.log(error);
+});
+  
+});
+
+
